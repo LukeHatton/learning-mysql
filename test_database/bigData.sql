91311f191a4fcc116f496041037282ccbc0d7836
Zhao Li <lukehatton685@gmail.com>
2021-12-04 11:58:23 +0800

    添加新的建表语句

--- test_database/bigData.sql
+++ test_database/bigData.sql
@@ -1,4 +1,42 @@
+CREATE DATABASE test_database;
+
 USE test_database;
+
+/* ================ 建表语句 ================= */
+-- auto-generated definition
+CREATE TABLE test_table
+(
+    id       INT AUTO_INCREMENT
+        PRIMARY KEY,
+    column_2 VARCHAR(64) NULL,
+    column_3 VARCHAR(64) NULL,
+    column_4 VARCHAR(64) NULL,
+    column_5 VARCHAR(64) NULL,
+    column_6 VARCHAR(64) NULL,
+    column_7 VARCHAR(64) NULL,
+    column_8 VARCHAR(64) NULL,
+    column_9 VARCHAR(64) NULL,
+    CONSTRAINT test_table_id_uindex
+        UNIQUE (id)
+)
+    COMMENT '测试表' ENGINE = MyISAM;
+
+CREATE TABLE test_table_uuid
+(
+    id       VARCHAR(36) NOT NULL
+        PRIMARY KEY,
+    column_2 VARCHAR(64) NULL,
+    column_3 VARCHAR(64) NULL,
+    column_4 VARCHAR(64) NULL,
+    column_5 VARCHAR(64) NULL,
+    column_6 VARCHAR(64) NULL,
+    column_7 VARCHAR(64) NULL,
+    column_8 VARCHAR(64) NULL,
+    column_9 VARCHAR(64) NULL,
+    row_num  INT         NULL
+)
+    COMMENT '主键为uuid的表';
+
 
 /*================ 测试查询速度 ================*/
 SELECT COUNT(*)
@@ -7,7 +45,7 @@ FROM test_table;
 EXPLAIN
 SELECT *
 FROM test_table
-    # LIMIT 900000,100;
+     # LIMIT 900000,100;
 WHERE id BETWEEN 900000 AND 900100;
 
 EXPLAIN
@@ -17,7 +55,7 @@ WHERE id = 99271;
 
 
 /*================ 主键为UUID的大量数据 ================*/
-DROP TABLE test_table_uuid;
+DROP TABLE IF EXISTS test_table_uuid;
 CREATE TABLE test_table_uuid
 (
     id       VARCHAR(36) NOT NULL,
@@ -35,16 +73,29 @@ CREATE TABLE test_table_uuid
     COMMENT '主键为uuid的表';
 
 CREATE UNIQUE INDEX test_table_uuid_id_uindex
-    ON test_table_uuid (id);
+    ON test_table_uuid_2 (id);
 
-ALTER TABLE test_table_uuid_2
+ALTER TABLE test_table_uuid
     ADD row_num INT NULL;
 
-CREATE UNIQUE INDEX test_table_uuid_2_row_num_uindex
+CREATE UNIQUE INDEX test_table_uuid_row_num_uindex
     ON test_table_uuid_2 (row_num);
 
+/* ================ 存储过程 ================= */
+CREATE PROCEDURE proc_insertFooData()
+BEGIN
+    DECLARE count INT DEFAULT 0;
+    loop_label: LOOP
+        IF count > 1000 * 1000 THEN
+            LEAVE loop_label;
+        ELSE
+            INSERT INTO test_table(column_2, column_3, column_4, column_5, column_6, column_7, column_8, column_9)
+            VALUES ('中文2', '中文3', '中文4', '中文5', '中文6', '中文7', '中文8', '中文9');
+            SET count = count + 1;
+        END IF;
+    END LOOP;
+END;
 
-/*---------------- 插入测试数据 ----------------*/
 # UUID(),中文2,中文3,中文4,中文5,中文6,中文7,中文8,中文9
 # 在insert中使用函数果然很慢，每秒平均插入量在一万左右，一百万数据插入花费两分钟
 DROP PROCEDURE IF EXISTS procedure_insert_foo_data;
@@ -53,7 +104,7 @@ CREATE PROCEDURE procedure_insert_foo_data()
 BEGIN
     DECLARE loop_index INTEGER DEFAULT 0;
     loop_label: LOOP
-        IF loop_index >= 1000000 THEN
+        IF loop_index >= 100000 THEN
             LEAVE loop_label ;
         END IF;
         INSERT INTO test_table_uuid (id, column_2, column_3, column_4, column_5, column_6, column_7, column_8, column_9)
@@ -63,6 +114,12 @@ BEGIN
 END %
 DELIMITER ;
 
+/*---------------- 插入测试数据 ----------------*/
+# windows平台mariadb居然花了20秒..每秒才五万插入
+CALL proc_insertFooData;
+
+# 10万主键UUID写入，花费两分钟
+# 相比之前的一万数据写入要花费40s，已经提升很大，但还是很慢，mac上写入百万只需要两分钟
 CALL procedure_insert_foo_data;
 
 SELECT LAST_INSERT_ID();
@@ -83,9 +140,9 @@ LIMIT 900000,30000;
 
 SELECT *
 FROM (
-    SELECT *, ROW_NUMBER() OVER (ORDER BY column_2) r
-    FROM test_table_uuid
-    LIMIT 100
+         SELECT *, ROW_NUMBER() OVER (ORDER BY column_2) r
+         FROM test_table_uuid
+         LIMIT 100
      ) t
 WHERE r BETWEEN 10 AND 20;
 
@@ -100,13 +157,42 @@ LIMIT 1000000;
 EXPLAIN
 SELECT *
 FROM test_table_uuid t
-    INNER JOIN
-(
-    SELECT id
-    FROM test_table_uuid
-    LIMIT 900000,30000
-)                    t2 ON t.id = t2.id;
+         INNER JOIN
+     (
+         SELECT id
+         FROM test_table_uuid
+         LIMIT 900000,30000
+     ) t2 ON t.id = t2.id;
 
 #
 SELECT id, ROW_NUMBER() OVER () AS 'row_number'
 FROM test_table_uuid;
+
+/* ---------------- 解决使用UUID插入数据非常慢的问题 ----------------- */
+SELECT *
+FROM test_table_uuid;
+# 查看一下存储引擎，看是不是存储引擎的问题
+SHOW CREATE TABLE test_table_uuid;
+# 会不会是UUID()函数执行太慢了？但是在mac上也用了这个函数，也是mariadb，速度就很快
+SELECT UUID();
+
+SELECT RAND() * 100;
+
+SELECT *
+FROM test_table;
+
+SELECT COUNT(*)
+FROM test_table_uuid;
+
+SHOW CREATE TABLE test_table_uuid;
+
+/* ================ 使用mysqlslap进行基准性能测试 ================= */
+# 创建测试表
+CREATE TABLE people
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(64) NULL,
+    age  INT         NULL
+);
+
+SHOW TABLES;
